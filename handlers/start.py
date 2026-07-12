351553bf4ab2bcfec35fc35267c14039cc14a876
nazarglabec-tech <nazarglabec@icloud.com>
2026-07-12 23:36:53 +0300

    Create start.py

---
+++ handlers/start.py
@@ -0,0 +1,12 @@
+from aiogram import Router
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+
+router = Router()
+
+@router.message(CommandStart())
+async def start(message: Message):
+    await message.answer(
+        "👋 Добро пожаловать!\n\n"
+        "Выберите нужный раздел:"
+    )
